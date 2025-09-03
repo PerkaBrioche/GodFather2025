b7f8cd8187679fbea9589af82a0b1d774b3887ca
ShaneDorin <shane.dorin@edu.devinci.fr>
2025-09-03 11:55:26 +0200

    [DEBUG] Safe Zone + Blood Zone detection
    RaycastDetector on the mouse will detect if the collider hover in the "Game Zone Raycasts" layer with the according tags.

--- Assets/RaycastDetector.cs
+++ Assets/RaycastDetector.cs
@@ -1,26 +1,30 @@
+using System.Drawing;
 using UnityEngine;
 
 public class RaycastDetector : MonoBehaviour
 {
     void Update()
     {
-        // Detect if mouse is hovering over a BloodZone or SafeZone tagged collider
-        
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+        RaycastHit2D pointHit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, LayerMask.GetMask("Game Zone Raycasts"));
 
-        if (hit.collider != null)
+
+        // Check if the object has the desired tag
+        if (pointHit.collider != null && pointHit.collider.CompareTag("SafeZone"))
         {
-            // Check if the object has the desired tag
-            if (hit.collider.CompareTag("SafeZone"))
-            {
-                Debug.Log("Safe");
-            }
-
-            if (hit.collider.CompareTag("BloodZone"))
-            {
-                Debug.Log("Blood!");
-            }
+            Debug.Log("Safe");
+        }
+        else if (pointHit.collider != null && pointHit.collider.CompareTag("BloodZone"))
+        {
+            Debug.Log("Blood!");
         }
     }
+
+    //void OnDrawGizmos()
+    //{
+    //    if (Camera.main == null) return;
+    //    Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //    Gizmos.color = UnityEngine.Color.red;
+    //    Gizmos.DrawWireSphere(mousePos, circleRadius);
+    //}
 }
